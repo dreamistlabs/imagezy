4d59897e49d9a41fdcdf23f8f59244f2c8c44085
Johnny Hu <heyitsjhu@gmail.com>
2017-08-24 10:25:39 -0700

    Create a custom SVG to serve as initial placeholder

---
+++ dist/imagezy.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var imgs = document.querySelectorAll('img.lazy');
+var placeholder = 'https://placehold.it/400x300';
+
+imgs.forEach(function (img) {
+  // if ((img.offsetTop > window.innerHeight) && !img.src) {
+  img.setAttribute('src', placeholder);
+  // } else {
+  //   img.setAttribute('src', img.getAttribute('data-src'));
+  // }
+  img.onload = function () {
+    console.log('onload');
+  };
+});
+window.addEventListener('scroll', function () {
+  imgs.forEach(function (img) {
+    if (img.offsetTop < window.innerHeight * .95 + window.scrollY && img.getAttribute('data-src')) {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.removeAttribute('data-src');
+    }
+  });
+});
--- src/imagezy.js
+++ src/imagezy.js
@@ -1,19 +1,42 @@
-var imgs = document.querySelectorAll('img.lazy');
-var placeholder = 'https://placehold.it/400x300';
+'use strict';
 
-imgs.forEach(function(img) {
-  // if ((img.offsetTop > window.innerHeight) && !img.src) {
-    img.setAttribute('src', placeholder);
-  // } else {
-  //   img.setAttribute('src', img.getAttribute('data-src'));
-  // }
-  img.onload = function() {
-    console.log('onload');
-  };
+const createPlaceholder = (width, height) => {
+  let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  let svgNS = svg.namespaceURI;
+  let shape = document.createElementNS(svgNS, "rect");
+
+  svg.setAttribute('width', width);
+  svg.setAttribute('height', height);
+  shape.setAttributeNS(null, "width", width);
+  shape.setAttributeNS(null, "height", height);
+  shape.setAttributeNS(null, "x", 0);
+  shape.setAttributeNS(null, "y", 0);
+  shape.setAttributeNS(null, "fill", "rgba(200, 200, 200, 0.8");
+  svg.appendChild(shape);
+  return svg;
+}
+
+const setImageToPlaceholder = (image, placeholder) => {
+  let xml = (new XMLSerializer).serializeToString(placeholder);
+  image.src = "data:image/svg+xml;charset=utf-8,"+xml;
+}
+/*!
+ * Collect all images to be lazy loaded.
+ */
+var imgs = document.querySelectorAll('img.lazy');
+
+/*!
+ * Load initial placeholder for each lazy image based on its width and height
+ */
+imgs.forEach((img) => {
+  let imgWidth = img.width, 
+      imgHeight = img.height;
+  let placeholder = createPlaceholder(imgWidth, imgHeight);
+  setImageToPlaceholder(img, placeholder);
 });
 window.addEventListener('scroll', function() {
   imgs.forEach(function(img) {
-    if (img.offsetTop < ((window.innerHeight * .95) + window.scrollY) && img.getAttribute('data-src')) {
+    if (img.offsetTop < ((window.innerHeight * .5) + window.scrollY) && img.getAttribute('data-src')) {
       img.setAttribute('src', img.getAttribute('data-src'));
       img.removeAttribute('data-src');
     }
