90621790febd4effcc8c3bdc2762b7c539c7ce24
Johnny Hu <heyitsjhu@gmail.com>
2017-08-31 22:36:37 -0700

    Inject imagezy CSS with javascript

--- src/imagezy.js
+++ src/imagezy.js
@@ -1,24 +1,7 @@
 'use strict';
 
 (function() {
-  /*!
-   * Create an SVG with the given width and height
-   */
-  const createPlaceholder = (width, height) => {
-    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    let svgNS = svg.namespaceURI;
-    let shape = document.createElementNS(svgNS, "rect");
 
-    svg.setAttribute('width', width);
-    svg.setAttribute('height', height);
-    shape.setAttributeNS(null, "width", width);
-    shape.setAttributeNS(null, "height", height);
-    shape.setAttributeNS(null, "x", 0);
-    shape.setAttributeNS(null, "y", 0);
-    shape.setAttributeNS(null, "fill", "rgba(200, 200, 200, 0.8");
-    svg.appendChild(shape);
-    return svg;
-  }
   /*!
    * Set an image's src attribute to an SVG
    */
@@ -51,12 +34,62 @@
   /*!
    * Create and append a new stylesheet to <head>
    */
-   let style = createStylesheet();
-   appendToHead(style);
-   let sheet = style.sheet;
-   sheet.insertRule(".lazy { width: 700px; }", 0);
-   console.log(sheet);
+  let style = createStylesheet();
+  appendToHead(style);
+  let sheet = style.sheet;
+  sheet.insertRule(
+  `div.imagezy {
+    position: relative;
+    display: inline-block;
+    width: 700px;
+    height: 400px;
+  }`, sheet.cssRules.length);
 
+  sheet.insertRule(
+  `div.imagezy:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    opacity: 1;
+    transition: all 2s;
+  }`, sheet.cssRules.length);
+
+  sheet.insertRule(
+  `img.imagezy-img {
+    height: 100%;
+    width: 100%;
+  }`, sheet.cssRules.length);
+
+  sheet.insertRule(
+  `div.imagezy.fadeOut:after {
+    animation: fadeOut 0.75s forwards;
+  }`, sheet.cssRules.length);
+
+  console.log(sheet.cssRules);
+
+  /*!
+   * Create an SVG with the given width and height
+   */
+  const createPlaceholder = (width, height) => {
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    let svgNS = svg.namespaceURI;
+    let shape = document.createElementNS(svgNS, "rect");
+
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+    shape.setAttributeNS(null, "width", width);
+    shape.setAttributeNS(null, "height", height);
+    shape.setAttributeNS(null, "x", 0);
+    shape.setAttributeNS(null, "y", 0);
+    shape.setAttributeNS(null, "fill", "rgba(200, 200, 200, 0.8");
+    svg.appendChild(shape);
+    return svg;
+  }
 
   /*!
    * Collect all images to be lazy loaded.
