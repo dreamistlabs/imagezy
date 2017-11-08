56dfbb096d4ca2b934fc28d11c4fa2bff79ae5e9
Johnny Hu <heyitsjhu@gmail.com>
2017-11-07 23:24:18 -0800

    Add content and functionality to tabs in getting started section

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -1,9 +1,34 @@
-var header = document.getElementById('site-header');
+let header = document.getElementById('site-header');
 
 window.addEventListener('scroll', function() {
-  var scrollPosition = window.scrollY;
+  let scrollPosition = window.scrollY;
 
   scrollPosition > 30
   ? header.classList.add('thin-header')
   : header.classList.remove('thin-header');
 });
+
+function switchTab(e) {
+  let _clickedTab = e;
+  let tabs = document.querySelectorAll('.tab-item');
+  let widgets = document.querySelectorAll('.widget');
+
+  if (!_clickedTab.classList.contains('is-active')) {
+    removeActiveStateFrom(tabs);
+    removeActiveStateFrom(widgets, _clickedTab, setActive);
+    _clickedTab.classList.add('is-active');
+  }
+}
+
+function removeActiveStateFrom(collection, reference, cb) {
+  for (let element of collection) {
+    element.classList.remove('is-active');
+    typeof cb == "function" && cb(element, reference);
+  }
+}
+
+function setActive(element, reference) {
+  if (element.getAttribute('data-widget-name') === reference.getAttribute('data-tab-name')) {
+    element.classList.add('is-active');
+  }
+}
